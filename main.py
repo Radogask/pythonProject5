65ea00b58298fede05853513514c3b3e23f2a0fa
radogask <stas.k2001@mail.ru>
2023-10-17 21:24:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#№21-667
+#Вариант 0
+def find_sum(array):
+    sum_of_elements = sum(array)
+    return sum_of_elements
+
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = find_sum(my_array)
+print("Сумма:", result)
